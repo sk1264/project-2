6d8f8da80c8b3d4497fc85293e5de8e135990b3c
sk1264 <samanthakaras1@gmail.com>
2023-04-10 11:18:31 -0400

    Controller routes added.

--- controllers/arts.js
+++ controllers/arts.js
@@ -4,12 +4,37 @@ const startArts = require('../db/artSeedData.js');
 const Art = require('../models/artwork.js');
 
 // Index
+router.get('/', async (req, res) => {
+    const art = await Art.find({});
+    res.render("arts/index.ejs", {art});
+});
 
 // Create
 
+router.get('/arts/new', (req, res) => {
+    res.render("arts/new.ejs")
+   })
+
+// Edit
+router.get('/:id/edit', async (req, res) => {
+	const art = await Art.findById(req.params.id);
+	res.render("arts/edit.ejs", {art})
+})
+
 // Post
 
+router.post('/', async (req, res) => {
+	console.log(req.body);
+	const art = await Art.create(req.body);
+	res.redirect('/arts');
+});
+
 // Show
+
+router.get('/:id', async (req, res) => {
+	const art = await Art.findById(req.params.id);
+	res.render("arts/show.ejs", {art})
+});
 
 // Delete
 
