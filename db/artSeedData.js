72b7ec785d7c00292593fc5b5896cc5798307148
sk1264 <samanthakaras1@gmail.com>
2023-04-10 21:11:01 -0400

    Playing around with styling.

--- db/artSeedData.js
+++ db/artSeedData.js
@@ -1,3 +1,7 @@
 module.exports = [
-    { name: 'Mona Lisa', location: 'Paris', image: 'none', review: 'beautiful', rating: 4, }
+    { name: 'Mona Lisa',
+    location: 'Paris',
+    image: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fe%2Fec%2FMona_Lisa%252C_by_Leonardo_da_Vinci%252C_from_C2RMF_retouched.jpg%2F1200px-Mona_Lisa%252C_by_Leonardo_da_Vinci%252C_from_C2RMF_retouched.jpg&tbnid=fnr9-q5vvXmRlM&vet=12ahUKEwja0LO9z6D-AhV3H2IAHTxoBiMQMygAegUIARDqAQ..i&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMona_Lisa&docid=8jbQaPwdqfR2tM&w=1200&h=1788&itg=1&q=mona%20lisa&ved=2ahUKEwja0LO9z6D-AhV3H2IAHTxoBiMQMygAegUIARDqAQ',
+    review: 'beautiful',
+    rating: 4, }
 ];
