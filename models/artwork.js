ccd7fdddc1651fd217224c04be998cac71f48f0c
sk1264 <samanthakaras1@gmail.com>
2023-04-15 10:27:24 -0400

    Added artist option for entry fields.

--- models/artwork.js
+++ models/artwork.js
@@ -3,6 +3,7 @@ const { mongoose } = require('../db/connection');
 const artsSchema = new mongoose.Schema({
     name: String,
     location: String, 
+    artist: String,
     image: String,
     review: String,
     rating: Number
