d74f8d93c2b17efdd9950e2c548f0e24138f0a22
sk1264 <samanthakaras1@gmail.com>
2023-04-22 08:17:03 -0400

    Finalizing all code.

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require('express');
 const morgan = require('morgan'); 
 const methodOverride = require('method-override');
 const app = express();
-const imgbbUploader = require("imgbb-uploader");
-const cors = require("cors");
 
 /////////////////////////////////////////////////////
 // Middleware  req => middleware => res
@@ -16,23 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-// app.use(cors())
-
-
-// app.post("/imageupload", (req, res, next) => {
-//   console.log("you've reached the imageupload route");
-//   console.log(req.body);
-//     const options = {
-//       apiKey: "2ebc72bcc7f50020878a9ea327a0b05a", // MANDATORY apikey for imgBB
-//       base64string: req.body.base64string,
-//       // OPTIONAL: pass base64-encoded image (max 32Mb)
-//     };
-//     imgbbUploader(options)
-//       .then((response) => {
-//         res.json(response);
-//       })
-//       .catch((error) => console.error(error));
-//   });
 
 app.get('/', (req, res) => {
     res.send('default route')
@@ -40,8 +21,6 @@ app.get('/', (req, res) => {
 
 const artsController = require('./controllers/arts');
 app.use('/arts', artsController);
-
-
 
 // Listener
 app.listen(process.env.PORT, () =>
