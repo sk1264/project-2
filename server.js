8e3bcbd3ab42d2679387d4bb7ef39dbb06359cf3
sk1264 <samanthakaras1@gmail.com>
2023-04-21 20:24:59 -0400

    Final edits

--- server.js
+++ server.js
@@ -16,23 +16,23 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(cors())
+// app.use(cors())
 
 
-app.post("/imageupload", (req, res, next) => {
-  console.log("you've reached the imageupload route");
-  console.log(req.body);
-    const options = {
-      apiKey: "2ebc72bcc7f50020878a9ea327a0b05a", // MANDATORY apikey for imgBB
-      base64string: req.body.base64string,
-      // OPTIONAL: pass base64-encoded image (max 32Mb)
-    };
-    imgbbUploader(options)
-      .then((response) => {
-        res.json(response);
-      })
-      .catch((error) => console.error(error));
-  });
+// app.post("/imageupload", (req, res, next) => {
+//   console.log("you've reached the imageupload route");
+//   console.log(req.body);
+//     const options = {
+//       apiKey: "2ebc72bcc7f50020878a9ea327a0b05a", // MANDATORY apikey for imgBB
+//       base64string: req.body.base64string,
+//       // OPTIONAL: pass base64-encoded image (max 32Mb)
+//     };
+//     imgbbUploader(options)
+//       .then((response) => {
+//         res.json(response);
+//       })
+//       .catch((error) => console.error(error));
+//   });
 
 app.get('/', (req, res) => {
     res.send('default route')
